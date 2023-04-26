d788b0b060e498acd21e3de25e56c5e9085f420a
iPoetDev <51715025+ipoetdev@users.noreply.github.com>
2023-04-26 17:47:36 +0100

    ChangeLog: 2nd Code Commit
    2023.04.24 00.00.001-001
    - Noted:
      - ADR003: Switched from Notion back to Google
      - Module: https://github.com/Code-Institute-Solutions/love-sandwiches-p5-sourcecode/tree/master/02-accessing-user-data
    - Bumped:
    - Added:
       - Added: connections.py for a module for connecting to Google
       - Added: settings.py for .env and global string settings
       - Added: data.py for data manipullation, validation functions
       - Added: logging to google-py.log (not shared)
    - Mutate:
       - Updated: refactored run.py to be the repl main function caller
       - Updated:
    - Removed:
    - ToDo:
    
    BREAKING CHANGE: - None

---
+++ connections.py
@@ -0,0 +1,118 @@
+#!/user/bin/env python3
+"""Module Connection Management."""
+
+# 0.1 Core Imports
+import logging
+import socket
+import ssl
+import sys
+
+# 0.2 Core Modules
+import gspread
+from google.oauth2.service_account import Credentials
+
+# 0.3 Local/Own Modules/Library
+from settings import DOMAINHOST, HOST, HTTPS, LOGS, SCOPE
+
+# Logging
+logging.basicConfig(filename=LOGS, level=logging.DEBUG)
+logging.captureWarnings(True)
+logging.info('Running on %s on port %d', HOST, HTTPS)
+
+
+def connect_to_remote():
+    """
+    Connects to a Google Sheet synchronously else exit for now
+    :return: collection
+    :rtype: NotionClient
+    """
+    try:
+        _creds = Credentials.from_service_account_file('creds.json')
+        # assert isinstance(_creds.with_scopes, SCOPE)
+        return _creds.with_scopes(SCOPE)
+    except Exception as _error:
+        _error_context: str = 'Exception'
+        _output: str = f'{_error_context}: {_error}'
+        logging.error(_output)
+        print(_output)
+        sys.exit(1)
+
+
+def get_source(credentials, file_name: str):
+    """
+    Connects to a Google Sheet synchronously else exit for now
+    :param credentials: Google sheet credentials
+    :type credentials: Credentials
+    :param file_name: Google sheet file name
+    :type file_name: str
+    :return: GSpreadClient
+    :rtype:
+    """
+    try:
+        _gsheet_client = gspread.authorize(credentials)
+        return _gsheet_client.open(file_name)
+    except Exception as _error:
+        _error_context: str = 'Exception'
+        _output: str = f'{_error_context}: {_error}'
+        logging.error(_output)
+        print(_output)
+        sys.exit(1)
+
+
+def open_sheet(file, tab: str):
+    """
+    Opens a Google sheet by tabname
+    :param file: Google sheet file
+    :type file: GSpreadClient
+    :param tab: Google sheet tab
+    :type tab: str
+    :return: worksheet
+    :rtype:
+    """
+    try:
+        return file.worksheet(tab)
+    except Exception as _error:
+        _error_context: str = 'Exception'
+        _output: str = f'{_error_context}: {_error}'
+        logging.error(_output)
+        print(_output)
+        sys.exit(1)
+
+
+def fetch_data(sheet):
+    """
+    Fetch the data from the Google sheet
+    :return: data
+    :rtype:
+    """
+    try:
+        return sheet.get_all_values()
+    except Exception as _error:
+        _error_context: str = 'Exception'
+        _output: str = f'{_error_context}: {_error}'
+        logging.error(_output)
+        print(_output)
+        sys.exit(1)
+
+
+def open_ssl_connection():
+    """
+    Opens secure connects to outside network
+    :return: _ssl
+    :rtype: SSLSocket
+    """
+    _connection = ssl.create_default_context()
+    _socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    _ssl = _connection.wrap_socket(_socket, server_hostname=DOMAINHOST)
+    _ssl.connect((DOMAINHOST, HTTPS))
+    return _ssl
+
+
+def close_ssl_connection(sslsock: ssl.SSLSocket):
+    """
+    Closes secure connects to outside network
+    :return:
+    :rtype:
+    """
+    sslsock.close()
+    sslsock = None
---
+++ settings.py
@@ -0,0 +1,58 @@
+#!/user/bin/env python3
+"""Module App Settings and Environmental Vars."""
+
+# @0min53
+# 0.1 Core Imports
+import logging
+# 0.2 Core Modules
+from pathlib import Path
+
+# String/Int Resources
+DOMAINHOST: str = 'www.google.com'
+HOST: str = 'www.googleapis.com'
+APIHOST: str = 'www.googleapis.com'
+HTTPS: int = 443
+LOGS: str = 'goggle-py.log'
+ENV: str = '.env'
+ENCODE: str = 'UTF-8'
+
+# String/Int Resources
+FILENAME: str = 'LoveSandwiches'
+TAB: str = 'sales'
+
+# Logging
+logging.basicConfig(filename=LOGS, level=logging.DEBUG)
+logging.captureWarnings(True)
+logging.info('Running on %s on port %d', HOST, HTTPS)
+
+SCOPE = [
+        "https://www.googleapis.com/auth/spreadsheets",
+        "https://www.googleapis.com/auth/drive.file",
+        "https://www.googleapis.com/auth/drive"
+        ]
+
+
+def load_env():
+    """
+    Load .env file to develop with env vars early.
+    """
+    try:
+        from dotenv import load_dotenv
+    except ModuleNotFoundError as notfound_error:
+        error_context: str = 'ModuleNotFoundError: dotenv'
+        issue_message: str = f'Could not load {ENV} file.'
+        guidance_message: str = f'Import python-dotenv to load external {ENV} or check the ENV file path. '
+        output: str = f'{error_context}: {issue_message} - {guidance_message}'
+        logging.error(output)
+        print(output)
+        logging.debug('Module: %s: as %r', str(notfound_error), notfound_error, exc_info=True)
+    else:
+        # With statement / Context management to load .env file and handle closure of path/files
+        success_message: str = f'Successfully loaded {ENV} file.'
+        # Get the path of the .env file
+        dotenv_path: Path = Path(ENV)
+        # Open the .env file and then load/close the .env file if a file exists
+        with dotenv_path.open(encoding=ENCODE) as dotenv_file:
+            load_dotenv(dotenv_path=dotenv_path)
+            logging.debug(success_message)
+        print(success_message)
